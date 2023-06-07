05116c9e19c36b82f6138cd6e5cd8dec39b0c35b
ducky <stefancohn@gmail.com>
2023-06-07 02:02:17 -0400

    Constants class created

---
+++ src/util/Constants.java
@@ -0,0 +1,16 @@
+package util;
+
+public class Constants {
+    //values to be used across whole game
+    public static final int SCREEN_WIDTH = 1000;
+    public static final int SCREEN_HEIGHT = 750;
+    public static final int FPS = 120;
+    public static final int UPS = 60;
+    public static final int SCALE = 3;
+    public static final int TILE_SIZE = 16 * SCALE;
+
+    public static final int DUCKY_RIGHT = 0;
+    public static final int DUCKY_LEFT = 0;
+    public static final int DUCKY_SPEED = 3; 
+
+}
